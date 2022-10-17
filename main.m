67ccb6302ef7f29b487a5f9a1d4854020720e8e2
Frida Marie Viset <frida.viset@gmail.com>
2022-10-17 12:04:55 +0200

    Add files via upload

---
+++ main.m
@@ -0,0 +1,13 @@
+%Copyright (C) 2022 by Frida Viset
+
+run('Illustrations\GenerateSurfaceIllustrations.m');
+run('Audio\ProcessAudioData.m');
+run('Precipitation\EstimateYearly.m');
+run('Precipitation\EstimateDailyGenerateTable.m');
+run('Precipitation\VaryAmountOfInducingPoints.m');
+run('BathymetryData\ProcessData.m');
+run('BathymetryData\ProcessDataFewerMeasurements.m');
+run('BathymetryData\ProcessDataFewerInducingPoints.m');
+
+
+
